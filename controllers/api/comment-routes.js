a76db0e4d59725cb681afe55866659f528cd33f7
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-07-10 19:18:15 -0500

    updated models

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const { Comment } = require('../../models/');
+const withAuth = require('../../utils/auth');
+
+// The `/api/comment` endpoint
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            userId: req.session.userId,
+        });
+        res.json(newComment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Update a comment
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const [affectedRows] = await Comment.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (affectedRows > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+
+});
+
+// Deletes a comment
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const [affectedRows] = await Comment.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (affectedRows > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
