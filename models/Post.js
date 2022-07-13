59cd39d55c90d75501334a42a144935c29c98e0c
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-07-13 17:08:33 -0500

    added back models folder

---
+++ models/Post.js
@@ -0,0 +1,26 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+class Post extends Model { }
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        body: {
+            type: DataTypes.STRING,
+        },
+    },
+    {
+        sequelize,
+    }
+);
+
+module.exports = Post;
