96e372f3473566a912b942bb121ba4bbe83ee8b5
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-07-13 13:19:22 -0500

    attempt to fix controllers issue and reran installs

--- controllers/user-routes.js
+++ controllers/user-routes.js
@@ -1,82 +1,70 @@
-const router = require("express").Router();
-const { User } = require("../../models");
+const router = require('express').Router();
+const { User } = require('../../models');
 
-router.post("/", (req, res) => {
-    User.create({
-        username: req.body.username,
-        password: req.body.password
-    })
-        .then(dbUserData => {
-            req.session.save(() => {
-                req.session.userId = dbUserData.id;
-                req.session.username = dbUserData.username;
-                req.session.loggedIn = true;
+// The `/api/user` endpoint
 
-                res.json(dbUserData);
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+// Creates a new user
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create({
+            username: req.body.username,
+            password: req.body.password,
         });
+
+        req.session.save(() => {
+            req.session.userId = newUser.id;
+            req.session.username = newUser.username;
+            req.session.loggedIn = true;
+
+            res.json(newUser);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.post("/login", (req, res) => {
-    User.findOne({
-        where: {
-            username: req.body.username
-        }
-    }).then(dbUserData => {
-        if (!dbUserData) {
+// Logs in a user
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+
+        if (!user) {
             res.status(400).json({ message: 'No user account found!' });
             return;
         }
 
-        const validPassword = dbUserData.checkPassword(req.body.password);
+        const validPassword = user.checkPassword(req.body.password);
 
         if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect password!' });
+            res.status(400).json({ message: 'No user account found!' });
             return;
         }
 
         req.session.save(() => {
-            req.session.userId = dbUserData.id;
-            req.session.username = dbUserData.username;
+            req.session.userId = user.id;
+            req.session.username = user.username;
             req.session.loggedIn = true;
 
-            res.json({ user: dbUserData, message: 'You are now logged in!' });
+            res.json({ user, message: 'You are now logged in!' });
         });
-    });
+    } catch (err) {
+        res.status(400).json({ message: 'No user account found!' });
+    }
 });
 
+// Logs out a user
 router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
             res.status(204).end();
         });
-    }
-    else {
+    } else {
         res.status(404).end();
     }
-});
-
-router.delete("/user/:id", (req, res) => {
-    User.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-        .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
 });
 
 module.exports = router;
